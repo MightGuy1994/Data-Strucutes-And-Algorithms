0c0da81ecd0bda6e8ffe285e6bca4faa893aafed
= <=>
2020-07-29 20:59:10 -0500

    Binary Search Tree Data Structure Implementation

---
+++ Trees/BinarySearchTree/Node.java
@@ -0,0 +1,52 @@
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data = data;
+    }
+
+    void insert(int value){
+        if(value <= data) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.insert(value);
+            }
+        }else{
+            if(right == null){
+                right = new Node(value);
+            }else{
+                right.insert(value);
+            }
+        }
+    }
+
+    boolean contains(int value){
+        if(value == data){
+            return true;
+        }
+        if(value < data){
+            if(left == null) {
+                return false;
+            } else {
+                return left.contains(value);
+            }
+        }else{
+            if(right ==null){
+                return false;
+            }else{
+                return right.contains(value);
+            }
+        }
+    }
+
+    void printInOrder(){
+        if(left !=null){
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if(right != null){
+            right.printInOrder();
+        }
+    }
+}
