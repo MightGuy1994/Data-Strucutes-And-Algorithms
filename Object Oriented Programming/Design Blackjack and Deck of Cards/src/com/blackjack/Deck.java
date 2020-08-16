d07079c0555f0123d151e03dbe0f43f0aec78e8a
= <kishoreakula1994@gmail.com>
2020-08-15 20:49:22 -0500

    Implemented Deck class. 1. added properties and funcations
    Signed-off-by: = <kishoreakula1994@gmail.com>

--- Object Oriented Programming/Design Blackjack and Deck of Cards/src/com/blackjack/Deck.java
+++ Object Oriented Programming/Design Blackjack and Deck of Cards/src/com/blackjack/Deck.java
@@ -1,4 +1,21 @@
 package com.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+    private List<BlackJackCard> cards;
+    Deck() {
+        this.cards = new ArrayList<>();
+        for (SUIT suit : SUIT.values()) {
+            for (int i = 1; i < 13; i++) {
+                cards.add(new BlackJackCard(suit, i));
+            }
+        }
+    }
+
+    public List<BlackJackCard> getCards() {
+        return cards;
+    }
 }
